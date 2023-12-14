2fc446bde5932ab04a99b3c121e0ad54f7046ccd
bountx <andrzej.pijanowski@gmail.com>
2023-12-14 11:43:58 +0100

    Renamed file

--- pwi_project/lib/view/notepad_screen_view.dart
+++ pwi_project/lib/view/notepad_screen_view.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
-class NotepadListScreen extends StatelessWidget {
-  const NotepadListScreen({super.key});
+class NotepadScreen extends StatelessWidget {
+  const NotepadScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Notepad List Screen'),
+        title: const Text('Notepad Screen'),
       ),
       body: const Center(
         child: Text(
-          'Welcome to the Notepad List Screen!',
+          'Welcome to the Notepad Screen!',
           style: TextStyle(fontSize: 20),
         ),
       ),
