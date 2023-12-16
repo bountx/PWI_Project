2084929c98e982d12d57ce6efc470120d81c572c
Kapponohacco <kacper.falinski5@gmail.com>
2023-12-17 00:00:09 +0100

    Added: - New button for creating new task - Test version for containers with tasks - Some colors(constant for now)

--- pwi_project/lib/view/task_manager_screen_view.dart
+++ pwi_project/lib/view/task_manager_screen_view.dart
@@ -1,20 +1,62 @@
 import 'package:flutter/material.dart';
 
 class TaskManagerScreen extends StatelessWidget {
-  const TaskManagerScreen({super.key});
+  TaskManagerScreen({super.key});
+
+  var tablica = [
+    Przycisk(),
+    Przycisk(),
+    Przycisk()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Task Manager Screen'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Task Manager Screen!',
-          style: TextStyle(fontSize: 20),
+      appBar: AppBar(),
+      backgroundColor: Color.fromRGBO(153, 217, 234, 1.0),
+      body: Center(
+        child: Column(
+            verticalDirection: VerticalDirection.down,
+            children: tablica,
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            tablica.add(Przycisk());
+          },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+          backgroundColor: Colors.blueAccent,
+          child: Container(
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            child: Text(
+              'New task',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 14.0),
+            ),
+          )),
     );
   }
+}
+
+Widget Przycisk() {
+  return Container(
+    width: 375,
+    padding: EdgeInsets.all(30.0),
+    decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.transparent),
+        color: Color.fromRGBO(235, 235, 140, 1.0),
+        borderRadius: BorderRadius.circular(20.0),
+
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text('wwwwwwwwwwwwwwwwwww'),
+        Text('pomocy'),
+      ],
+    ),
+  );
 }
