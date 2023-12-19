e00eeb8c05cbe78d20374a9071a305897354f685
bountx <andrzej.pijanowski@gmail.com>
2023-12-19 22:45:53 +0100

    Edited DataSource for notes and tasks

--- pwi_project/lib/view/calendar_screen_view.dart
+++ pwi_project/lib/view/calendar_screen_view.dart
@@ -3,28 +3,30 @@ import 'package:provider/provider.dart';
 import 'package:pwi_project/view_model/calendar_view_model.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
+import '../view_model/note_view_model.dart';
 import '../view_model/task_view_model.dart';
 
 class CalendarScreen extends StatelessWidget {
-  CalendarScreen({super.key});
+  const CalendarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calendar Screen'),
-      ),
-      body: SfCalendar(
-        onSelectionChanged: (selection){
-          print(Provider.of<Calendar>(context, listen: false).controller.selectedDate);
-        },
-        controller: Provider.of<Calendar>(context).controller,
-        view: CalendarView.month,
-        dataSource: TasksDataSource(Provider.of<TaskList>(context).exampleTasks),
-        monthViewSettings: MonthViewSettings(
-          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+        appBar: AppBar(
+          title: const Text('Calendar Screen'),
         ),
-      )
-    );
+        body: SfCalendar(
+          onSelectionChanged: (selection) {},
+          controller: Provider.of<Calendar>(context).controller,
+          dataSource: DataSource(
+              Provider.of<TaskList>(context).exampleTasks,
+              Provider.of<NoteList>(context).exampleNotes),
+          monthViewSettings: const MonthViewSettings(
+            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+            showAgenda: true,
+          ),
+          showDatePickerButton: true,
+          view: CalendarView.month,
+        ));
   }
 }
--- pwi_project/lib/view_model/calendar_view_model.dart
+++ pwi_project/lib/view_model/calendar_view_model.dart
@@ -2,25 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:pwi_project/view_model/task_view_model.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
+import 'note_view_model.dart';
+
 class Calendar extends ChangeNotifier {
   final _controller = CalendarController();
 
   CalendarController get controller => _controller;
 }
 
-class TasksDataSource extends CalendarDataSource {
-  TasksDataSource(List<Task> source) {
+class DataSource extends CalendarDataSource {
+  DataSource(List<Task> taskSource, List<Note> noteSource) {
     appointments = [];
-    for(Task task in source){
-      Appointment appointment = Appointment(
+    for(Task task in taskSource){
+      Appointment taskAppointment = Appointment(
         subject: task.name,
         startTime: task.day,
-        endTime: task.day.add(const Duration(hours: 2)),
+        endTime: task.day,
         color: task.background,
         notes: task.description,
         isAllDay: true,
       );
-      appointments?.add(appointment);
+      appointments?.add(taskAppointment);
+    }
+    for(Note note in noteSource){
+      Appointment noteAppointment = Appointment(
+        subject: note.name,
+        startTime: note.day,
+        endTime: note.day,
+        color: note.background,
+        notes: note.description,
+        isAllDay: true,
+      );
+      appointments?.add(noteAppointment);
     }
   }
 }
