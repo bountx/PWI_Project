864e780c348c12026910073004fbf6a3597b8f01
bountx <andrzej.pijanowski@gmail.com>
2023-12-19 12:27:11 +0100

    Added data source to calendar

--- pwi_project/lib/view/calendar_screen_view.dart
+++ pwi_project/lib/view/calendar_screen_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pwi_project/view_model/calendar_view_model.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarScreen extends StatelessWidget {
-  const CalendarScreen({super.key});
+  CalendarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +12,17 @@ class CalendarScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Calendar Screen'),
       ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Calendar Screen!',
-          style: TextStyle(fontSize: 20),
+      body: SfCalendar(
+        onSelectionChanged: (selection){
+          print(Provider.of<Calendar>(context, listen: false).controller.selectedDate);
+        },
+        controller: Provider.of<Calendar>(context).controller,
+        view: CalendarView.month,
+        dataSource: Provider.of<Calendar>(context).getTasksDataSource(context),
+        monthViewSettings: const MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
         ),
-      ),
+      )
     );
   }
 }
---
+++ pwi_project/lib/view_model/calendar_view_model.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pwi_project/view_model/task_view_model.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class Calendar extends ChangeNotifier {
+  final _controller = CalendarController();
+
+  CalendarDataSource getTasksDataSource(BuildContext context){
+    List<Task> tasks = Provider.of<TaskList>(context).exampleTasks;
+    return TasksDataSource(tasks);
+  }
+
+  CalendarController get controller => _controller;
+}
+
+class TasksDataSource extends CalendarDataSource {
+  TasksDataSource(List<Task> source) {
+    appointments = [];
+    for(Task task in source){
+      Appointment appointment = Appointment(
+        subject: task.name,
+        startTime: task.day,
+        endTime: task.day.add(const Duration(hours: 2)),
+        color: task.background,
+        notes: task.description,
+        isAllDay: true,
+      );
+      appointments?.add(appointment);
+    }
+  }
+}
