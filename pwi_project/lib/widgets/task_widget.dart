1329b3a0458d15b6a07c87b59e6ab596ad6a4ec7
Kapponohacco <kacper.falinski5@gmail.com>
2023-12-20 21:53:21 +0100

    Created a TaskWidget's base appearance model and made it clickable

--- pwi_project/lib/widgets/task_widget.dart
+++ pwi_project/lib/widgets/task_widget.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:pwi_project/view_model/task_view_model.dart';
 
 class TaskWidget extends StatelessWidget {
-  TaskWidget(this.name, this.description, this.day, this.background, this.isDone, this.index, {super.key});
+  TaskWidget(this.name, this.description, this.day, this.background,
+      this.isDone, this.index,
+      {super.key});
 
   String name;
   String description;
@@ -13,8 +16,27 @@ class TaskWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(name),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[Container(
+            margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+            width: 300,
+            height: 90,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color:
+              Provider.of<TaskList>(context, listen: false).tasks.last.background,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: GestureDetector(
+              onTap: (){
+                print(Provider.of<TaskList>(context, listen: false).tasks.last.description);
+                //placeholder! Goal: Should provide a tasks description, but need indexes for it
+              },
+            )
+      ),
+
+    ],
     );
   }
 }
