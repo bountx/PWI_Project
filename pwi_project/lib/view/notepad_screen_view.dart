70961d8e71a2ff7a51aa4d2aeefcd659399d7ef0
Haftan <j.haftan666@gmail.com>
2023-12-20 15:38:11 +0100

    infinite list of containers + add button
    next step is to change containers into note displayers, no logic yet

--- pwi_project/lib/view/notepad_screen_view.dart
+++ pwi_project/lib/view/notepad_screen_view.dart
@@ -10,14 +10,26 @@ class NotepadScreen extends StatelessWidget {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
+        backgroundColor: Colors.orange[50],
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(kToolbarHeight),
           child: AppBar(
             title: Row(
               children: [
-                IconButton(
-                  icon: Icon(Icons.format_list_bulleted),
-                  onPressed: () {},
+                Material(
+                  color: Colors.yellowAccent,
+                  borderRadius: BorderRadius.circular(10),
+                  child: InkWell(
+                    onTap: () {
+                      // handle the tap event here
+                    },
+                    borderRadius: BorderRadius.circular(10),
+                    splashColor: Colors.amberAccent[100], // change this to your desired splash color
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0), // adjust padding as needed
+                      child: Icon(Icons.format_list_bulleted),
+                    ),
+                  ),
                 ),
                 Expanded(
                   child: Container(
@@ -38,14 +50,30 @@ class NotepadScreen extends StatelessWidget {
                 ),
               ],
             ),
-            backgroundColor: Colors.limeAccent,
+            backgroundColor: Colors.amberAccent[400],
           ),
         ),
-        body: const Center(
-          child: Text(
-            'Welcome to the Notepad Screen!',
-            style: TextStyle(fontSize: 20),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.limeAccent,
+          child: const Icon(
+            Icons.add,
           ),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              height: MediaQuery.of(context).size.height / 4 - 30,
+              margin: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+              decoration: BoxDecoration(
+                color: Colors.amber[200],
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Text('Container $index'),
+              ),
+            );
+          },
         ),
       ),
     );
