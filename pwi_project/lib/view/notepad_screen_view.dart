2d55b46a54c39a20e757014081aa8647ec529317
Haftan <j.haftan666@gmail.com>
2023-12-20 14:38:58 +0100

    List view button and Search bar added
    not working yet, just visual representation

--- pwi_project/lib/view/notepad_screen_view.dart
+++ pwi_project/lib/view/notepad_screen_view.dart
@@ -5,14 +5,47 @@ class NotepadScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notepad Screen'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to the Notepad Screen!',
-          style: TextStyle(fontSize: 20),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: AppBar(
+            title: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.format_list_bulleted),
+                  onPressed: () {},
+                ),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        contentPadding: EdgeInsets.all(10),
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.limeAccent,
+          ),
+        ),
+        body: const Center(
+          child: Text(
+            'Welcome to the Notepad Screen!',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
     );
