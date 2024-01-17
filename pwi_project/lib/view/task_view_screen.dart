97303d075af5d36bda58ca6ff6dfebbef59f4341
Ania <150084251+Andybellcia@users.noreply.github.com>
2024-01-17 16:30:01 +0100

    Changed appearance of task view screen
    Co-Authored-By: Kapponohacco <94643947+Kapponohacco@users.noreply.github.com>

--- pwi_project/lib/view/task_view_screen.dart
+++ pwi_project/lib/view/task_view_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
 import '../model/task.dart';
 import '../view_model/task_creation_view_model.dart';
 import '../view_model/task_view_model.dart';
@@ -8,8 +9,8 @@ import 'task_edit_screen_view.dart';
 
 class TaskViewWidget extends StatelessWidget {
   final Task task;
-  TaskViewWidget({required this.task});
 
+  TaskViewWidget({required this.task});
 
   @override
   Widget build(BuildContext context) {
@@ -27,50 +28,94 @@ class TaskViewWidget extends StatelessWidget {
             ),
             actions: [
               Row(
-                children:[
+                children: [
                   IconButton(
                     icon: Icon(Icons.delete),
                     onPressed: () {
-                      Provider.of<TaskList>(context, listen: false).removeTask(task.id);
+                      Provider.of<TaskList>(context, listen: false)
+                          .removeTask(task.id);
                       Navigator.pop(context);
                     },
-
-                      ),
-                      IconButton(
-                      icon:
-                       Icon(Icons.edit),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => TaskEditWidget(task: task,))
-                      );
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TaskEditWidget(
+                                    task: task,
+                                  )));
                     },
                   ),
-              ],
+                ],
               ),
             ],
           ),
           body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child:
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Finish Date:'),
-                ElevatedButton(
-                  onPressed: () {
-                    model.viewcalendar(context);
-                  },
-                  child: Text(DateFormat('yyyy-MM-dd HH:mm').format(model.selectedDate.toLocal())),
-                ),
-                Text('Description:'),
-                Expanded(
-                  child: Text(task.description),
-                )
-              ],
-            ),
-            ),
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                          ),
+                            'Finish Date'
+                        ),
+                        Text(
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
+                            DateFormat('yyyy-MM-dd HH:mm').format(model.selectedDate.toLocal())),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 650, // media query
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: task.background,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: SingleChildScrollView(
+                      child: Container(
+                        margin: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Description',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 27.0,
+                              ),
+                            ),
+                            Text(
+                              task.description,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
           ),
         ),
+      ),
     );
   }
 }
